ca2b173406efed06cbde5b6b888dd3f4948a764f
Ryan White <5983348+rprwhite@users.noreply.github.com>
2023-10-04 21:43:26 +0800

    v2.1.3

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
     '@remix-run/eslint-config',
     'plugin:hydrogen/recommended',
     'plugin:hydrogen/typescript',
-    'prettier',
   ],
   rules: {
     '@typescript-eslint/ban-ts-comment': 'off',
