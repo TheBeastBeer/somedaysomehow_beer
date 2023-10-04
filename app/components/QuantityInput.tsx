2aa417c080e22b606819d9999642fb0a59d73650
Ryan White <5983348+rprwhite@users.noreply.github.com>
2023-10-04 21:04:10 +0800

    Make input read only, stop browser complaining

--- app/components/QuantityInput.tsx
+++ app/components/QuantityInput.tsx
@@ -63,6 +63,7 @@ export function QuantityInput() {
           type="number"
           id="Quantity"
           value={count}
+          readOnly
           className="text-black h-10 w-16 border-transparent text-center [-moz-appearance:_textfield] sm:text-sm [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none"
         />
 
